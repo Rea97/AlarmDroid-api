e6f20318a3b04333f7dc03161c230d29c0b72906
Oziel Martinez <ozielrea97@hotmail.com>
2018-03-04 15:51:13 -0600

    Add relations between current models

--- app/Alert.php
+++ app/Alert.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Alert extends Model
 {
-    //
+    /**
+     * One to many relation with Robot.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function robot()
+    {
+        return $this->belongsTo(Robot::class);
+    }
 }
--- app/Robot.php
+++ app/Robot.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Robot extends Model
 {
-    //
+    /**
+     * One to one relation with User.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * One to many relation with Alert.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function alerts()
+    {
+        return $this->hasMany(Alert::class);
+    }
 }
