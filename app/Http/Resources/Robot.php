30579a63960afffcab5d50a4c1a744b12f80c83f
Oziel Martinez <ozielrea97@hotmail.com>
2018-03-04 17:24:10 -0600

    Add RobotResource

---
+++ app/Http/Resources/Robot.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Robot extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'user' => $this->whenLoaded('user', new User($this->user), $this->user_id),
+            'model' => $this->model,
+            'zone' => $this->zone,
+            'created_at' => (string) $this->created_at,
+        ];
+    }
+}
