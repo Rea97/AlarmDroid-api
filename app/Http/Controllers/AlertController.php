5ac078fd457061449d8554e1b0235ff21bf9dfac
Oziel Martinez <ozielrea97@hotmail.com>
2018-03-04 18:17:14 -0600

    Add restriction to index method in AlertController
    Now only fetch alerts created by user's robot

--- app/Http/Controllers/AlertController.php
+++ app/Http/Controllers/AlertController.php
@@ -11,7 +11,7 @@ class AlertController extends Controller
     public function index(Request $request)
     {
         $search = $request->query('search');
-        $alerts = Alert::paginate(5);
+        $alerts = Alert::where('robot_id', $request->user()->robot->id)->paginate(5);
 
         if ($request->filled('search')) {
             $alerts = Alert::where('type', 'LIKE', "%{$search}%")->orWhere('message', 'LIKE', "%{$search}%")->paginate(5);
