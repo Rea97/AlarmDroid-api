20c19f70a8b56ac99fbf3b30909ee253ee2015f7
Oziel Martinez <ozielrea97@hotmail.com>
2018-03-04 18:12:03 -0600

    Add auth middleware to robot and alert routes

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('/login', 'ApiLoginController@login');
 
-// Robot routes
-Route::get('/robots/{robot}', 'RobotController@show')->name('robots.show');
+Route::middleware('auth:api')->group(function () {
+    // Robot routes
+    Route::get('/robots/{robot}', 'RobotController@show')->name('robots.show');
 
 // Alerts routes
-Route::get('/alerts', 'AlertController@index')->name('alerts.index');
-Route::get('/alerts/{alert}', 'AlertController@show')->name('alerts.show');
+    Route::get('/alerts', 'AlertController@index')->name('alerts.index');
+    Route::get('/alerts/{alert}', 'AlertController@show')->name('alerts.show');
+});
