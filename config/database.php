87f1d39330913e3a35dae2526aec591a58b5a6c3
Oziel Martinez <ozielrea97@hotmail.com>
2018-03-05 21:23:45 -0600

    Add heroku_pgsql database connection

--- config/database.php
+++ config/database.php
@@ -67,6 +67,18 @@ return [
             'sslmode' => 'prefer',
         ],
 
+        'heroku_pgsql' => [
+            'driver' => 'pgsql',
+            'host'     => parse_url(getenv('DATABASE_URL'), PHP_URL_HOST),
+            'database' => substr(parse_url(getenv('DATABASE_URL'), PHP_URL_PATH), 1),
+            'username' => parse_url(getenv('DATABASE_URL'), PHP_URL_USER),
+            'password' => parse_url(getenv('DATABASE_URL'), PHP_URL_PASS),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'host' => env('DB_HOST', 'localhost'),
